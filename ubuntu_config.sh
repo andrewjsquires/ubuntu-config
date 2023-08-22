9dca12cd2da2daee089c5f2959e22e49099bebff
andy <andrewjsquires@gmail.com>
2023-08-22 09:22:49 +0100

    added install for vscode & aws cli, maximise screen, UK keyboard

--- ubuntu_config.sh
+++ ubuntu_config.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo Clean up Chrome installer
+rm google-chrome-stable_current_amd64.deb
+
  sudo apt-get update
 
- declare -a arr=("atom" "curl" "docker.io" "gnome-shell-extension-prefs" "dconf-editor" "gnome-tweaks", "zsh")
+ declare -a arr=("curl" "docker.io" "gnome-shell-extension-prefs" "dconf-editor" "gnome-tweaks", "zsh")
  for name in "${arr[@]}"
  do
     dpkg -s $name &> /dev/null
@@ -18,22 +21,38 @@
     fi
  done
 
-#install postman
+echo install vscode
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+echo install postman
 curl -o- "https://dl-cli.pstmn.io/install/linux64.sh" | sh
 
-#get ohmyzsh
+echo get ohmyzsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
- # name="aws"
- # dpkg -s $name  &> /dev/null
- #
- # if [ $? -ne 0 ]
- #
- #     then
- #         echo "$name - installing"
- #         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
- #         unzip awscliv2.zip
- #         sudo ./aws/install
- #     else
- #         echo "$name - already installed"
- # fi
+echo Change keyboard to uk
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'gb')]"
+
+echo make screen full size
+cd /media/andy/VBox_GAs_7.0.10/
+./autorun.sh
+
+
+echo install aws cli
+ name="aws"
+ dpkg -s $name  &> /dev/null
+ 
+ if [ $? -ne 0 ]
+ 
+     then
+         echo "$name - installing"
+         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+         unzip awscliv2.zip
+         sudo ./aws/install
+     else
+         echo "$name - already installed"
+ fi
