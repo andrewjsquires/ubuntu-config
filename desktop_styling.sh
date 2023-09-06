e35b4673f2eb8d66fe0a23b2d5f45875b257f145
andy <andrewjsquires@gmail.com>
2023-09-06 09:34:57 +0100

    added desktop styling

--- desktop_styling.sh
+++ desktop_styling.sh
@@ -6,7 +6,10 @@ gsettings set org.gnome.desktop.screensaver lock-enabled false
 echo remove desktop items
 gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
 gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+gsettings set org.gnome.desktop.background show-desktop-icons false
 
+echo turn off calendar notifications
+gsettings set org.gnome.evolution-data-server.calendar notify-with-tray false
 
 echo change to dark 
 gsettings set org.gnome.shell.ubuntu color-scheme prefer-dark
@@ -23,4 +26,18 @@ sudo apt update
 sudo apt install -y flat-remix-gnome
 
 echo moving launcher to bottom
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
+settings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
+gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items true
+# gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+# gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
+
+# echo add favourites
+# gsettings set org.gnome.shell favorite-apps  "['org.gnome.Nautilus.desktop', 'snap-store_ubuntu-software.desktop', 'org.gnome.Terminal.desktop', 'code_code.desktop', 'google-chrome.desktop', 'org.gnome.gedit.desktop', 'postman_postman.desktop']"
+
+
+echo changing background
+sudo cp ./img/* /usr/share/backgrounds
+gsettings set org.gnome.desktop.background picture-uri-dark file:////usr/share/backgrounds/nightsky.jpg
